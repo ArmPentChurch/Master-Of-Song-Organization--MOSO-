392371d8d79b0a44979b228a2cd6d731c76409d4
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2025-08-04 20:31:54 -0700

    Update SPOT

--- SPOT/__init__.py
+++ SPOT/__init__.py
@@ -11,5 +11,7 @@ Otherwise known as a place to keep varibales which are meat to be constant ie: u
 import json
 with open(".moso", 'r', encoding='utf-8') as session_file:
     session_data = json.load(session_file)
-    DATABASE_FILEPATH = session_data["database"]
-    ERGER_DIRECTORY = session_data["erger_directory"]
+    DATABASE_FILEPATH:str = session_data["database"]
+    ERGER_DIRECTORY:str = session_data["erger_directory"]
+    OUTPUT_FOLDER:str =  session_data["output_folder"]
+    PAST_SONGS_FILEPATH:str = session_data["past_songs"]
