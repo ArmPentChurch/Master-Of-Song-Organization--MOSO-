f1d540ee854459c00aa5a657385044038d88c7e3
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2025-08-04 17:00:41 -0700

    Got rid of useless code

--- getAllLyrics.py
+++ getAllLyrics.py
@@ -2,8 +2,6 @@ from time import time as today
 from json import load
 from os import environ as ENV
 from docx import Document
-from regex import D
-basePth = ENV.get("OneDrive")
 #open all files, get latest version and store lyrics in dict
 # if songLyrics.get('latestChange',None) or do < today()
 
